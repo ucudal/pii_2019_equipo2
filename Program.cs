dbabb9de1eab653034997738acedea981fed7b66
SanGar03 <48578485+SanGar03@users.noreply.github.com>
2019-03-29 21:01:42 -0300

    Santiago Garcia

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
+
 <<<<<<< HEAD
 =======
-﻿//Juan Manuel Beracochea De Leon, ﻿Gonzalo Sempol
+﻿//Juan Manuel Beracochea De Leon, ﻿Gonzalo Sempol, Santiago Garcia
 >>>>>>> 15a270c2f55a879cf7d1a15873c775b61210ccb4
 using System;
 using System.Linq;
