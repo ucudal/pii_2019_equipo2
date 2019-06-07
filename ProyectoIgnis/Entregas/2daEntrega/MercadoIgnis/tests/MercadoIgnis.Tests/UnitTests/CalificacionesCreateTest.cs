5458bdb44b0dc249326f1433bb01621c36d9af6a
Gonzalo Sempol <gonzalo.sempol@gmail.com>
2019-06-07 20:53:58 -0300

    Imagensitas

--- ProyectoIgnis/Entregas/2daEntrega/MercadoIgnis/tests/MercadoIgnis.Tests/UnitTests/CalificacionesCreateTest.cs
+++ ProyectoIgnis/Entregas/2daEntrega/MercadoIgnis/tests/MercadoIgnis.Tests/UnitTests/CalificacionesCreateTest.cs
@@ -36,11 +36,13 @@ namespace MercadoIgnis.Tests.UnitTests
             Calificacion CalificacionEsperada = new Calificacion(){ID=1,Nota=5,Descripcion="Descripcion de prueba"};
            
             // Act
-            //Creamos una pagina de tipo CreateModel (de Calificaciones), la cual es la que se encarga de la logica de crear calificaciones en bd.
+            //Creamos una pagina de tipo CreateModel (de Calificaciones), la cual es la que se encarga de la logica 
+            //de crear calificaciones en bd.
             CreateModel pageCreateModel = new CreateModel(testMercadoIgnisContext);
                       
           
-            //Introducimos una calificacion en el modelo de la pagina que creamos, a mano Seteamos los valores de la calificacion de esa página
+            //Introducimos una calificacion en el modelo de la pagina que creamos, a mano Seteamos los valores de 
+            //la calificacion de esa página
             pageCreateModel.Calificacion = new Calificacion(){ID=1,Nota=5,Descripcion="Descripcion de prueba"};
             
            
@@ -51,7 +53,8 @@ namespace MercadoIgnis.Tests.UnitTests
             //Buscamos usando el contexto la Calificacion recien creada por id
             Calificacion CalificacionRecibida = await testMercadoIgnisContext.Calificacion.FindAsync(1);
             
-            //Comparamos que la que creamos en el modelo de la pagina y por ende mandamos a crear en bd, y la calificacion que recibimos de bd con id 1, tengan igual descripcion y nota
+            //Comparamos que la que creamos en el modelo de la pagina y por ende mandamos a crear en bd, 
+            //y la calificacion que recibimos de bd con id 1, tengan igual descripcion y nota
             Assert.Equal(
                 CalificacionEsperada.Descripcion.ToString(), 
                 CalificacionRecibida.Descripcion.ToString());
@@ -59,7 +62,8 @@ namespace MercadoIgnis.Tests.UnitTests
                 CalificacionEsperada.Nota.ToString(), 
                 CalificacionRecibida.Nota.ToString());
             
-            //Si esto no falla, concluimos que la pagina de calificaciones (de tener bien seteado el modelo), guarda sin problemas una calificacion en bd cuando no hay nada ingresado
+            //Si esto no falla, concluimos que la pagina de calificaciones (de tener bien seteado el modelo), 
+            //guarda sin problemas una calificacion en bd cuando no hay nada ingresado
 
             
            
