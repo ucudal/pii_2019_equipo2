415d04931183cf5c8f6fbeeaa7357897030530cc
SanGar03 <48578485+SanGar03@users.noreply.github.com>
2019-07-04 23:54:15 -0300

    Ultimo master

--- ProyectoIgnis/Entregas/3erEntrega/MercadoIgnis/src/MercadoIgnis/Migrations/20190705025227_InitialCreateIdentity.Designer.cs
+++ ProyectoIgnis/Entregas/3erEntrega/MercadoIgnis/src/MercadoIgnis/Migrations/20190705025227_InitialCreateIdentity.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MercadoIgnis.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    [Migration("20190705021925_InitialCreateIdentity")]
+    [Migration("20190705025227_InitialCreateIdentity")]
     partial class InitialCreateIdentity
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
